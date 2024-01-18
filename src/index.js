fdfcc5c0aabfbe20b681b0ffe6991c3fb0aa7fb9
AmazingAJ10 <allenjohnhobbs5792@gmail.com>
2024-01-18 12:18:00 -0500

    https://github.com/CSNSE/subs5/issues/2

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Amplify } from 'aws-amplify';
 import config from './aws-exports';
-import "@aws-amplify/ui-react/styles.css"
+import "@aws-amplify/ui-react/styles.css"  // new css reference
 Amplify.configure(config);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
